0e58e806d84c1d8f57f27155dfcf127082ba53e5
Jan Zimmer <galileo@macedobuech.home>
2025-04-03 11:57:40 +0200

    added CMadd9 to soundcloud

--- src/data/links.ts
+++ src/data/links.ts
@@ -55,6 +55,12 @@ export const LINKS: Link[] = [
       {
         id: "sc-1",
         embedUrl:
+          "https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/2069761596&color=%23ff5500&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true&visual=true",
+        title: `Cmadd9🌟`,
+      },
+      {
+        id: "sc-2",
+        embedUrl:
           "https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/1748825088&color=%23ff5500&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true&visual=true",
         title: `Cover of "Idea 10" - by @Gibran Alcocer`,
       },
